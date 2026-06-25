e5892e0aa9924f465344cda9c7058d18d9ec3715
Lightaceing <lightaceing1@gmail.com>
2026-06-25 17:54:53 +0530

    Changed and implemented single file structure.

---
+++ JS/top_bar.js
@@ -0,0 +1,53 @@
+class TabBar extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="tab">
+      <!----For the top tab-->
+
+      Tanveer Ahmed Khan - System Engineer
+
+      <button>
+        <a href="/about-me.html" style="text-decoration: none">About Me</a>
+      </button>
+
+      <button>
+        <a href="/index.html" style="text-decoration: none">Home</a>
+      </button>
+
+      <button>
+        <a href="/project-list.html" style="text-decoration: none">Projects</a>
+      </button>
+
+        <button>
+        <a href="/Assets/Docs/Tanveer_Ahmed_Khan_6_2026.pdf" style="text-decoration: none">Resume</a>
+      </button>
+
+      <button>
+        <a href="https://www.linkedin.com/in/tanveer-a-khan/">
+          <img
+            src="/Assets/linkedin_icon.svg"
+            alt="LinkedIn"
+            class="resume_icon"
+          />
+        </a>
+      </button>
+
+      <button>
+        <a href="https://github.com/tanveer-akhan">
+          <img src="/Assets/github_icon.svg" alt="Github" class="resume_icon" />
+        </a>
+      </button>
+
+
+
+      <button>
+        <a href="https://www.kaggle.com/lightaceing">
+          <img src="/Assets/kaggle_icon.svg" alt="Kaggle" class="resume_icon" />
+        </a>
+      </button>
+    </div>
+    `;
+  }
+}
+
+customElements.define("top-bar", TabBar);
