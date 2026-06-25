3b52fcdcb18f8c7e23694893122e4a6f0f575302
Lightaceing <lightaceing1@gmail.com>
2026-06-25 21:10:57 +0530

    Improved fonts.

--- JS/top_bar.js
+++ JS/top_bar.js
@@ -1,50 +1,62 @@
 class TabBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-    <div class="tab">
-      <!----For the top tab-->
-
-      Tanveer Ahmed Khan - System Engineer
-
-      <button>
-        <a href="/about-me.html" style="text-decoration: none">About Me</a>
-      </button>
-
-      <button>
-        <a href="/index.html" style="text-decoration: none">Home</a>
-      </button>
-
-      <button>
-        <a href="/project-list.html" style="text-decoration: none">Projects</a>
-      </button>
+        <!-- top bar -->
+    <div class="top-bar">
+      <span class="name"> Tanveer Ahmed Khan - System Engineer</span>
+      <div class="nav-buttons">
+        <button>
+          <a href="mailto:tanveer.akhan.work@gmail.com" style="text-decoration: none">Email Me</a>
+        </button>
 
         <button>
-        <a href="/Assets/Docs/Tanveer_Ahmed_Khan_6_2026.pdf" style="text-decoration: none">Resume</a>
-      </button>
+          <a href="/index.html" style="text-decoration: none">Home</a>
+        </button>
 
-      <button>
-        <a href="https://www.linkedin.com/in/tanveer-a-khan/">
-          <img
-            src="/Assets/linkedin_icon.svg"
-            alt="LinkedIn"
-            class="resume_icon"
-          />
-        </a>
-      </button>
+        <button>
+          <a href="/project-list.html" style="text-decoration: none"
+            >Projects</a
+          >
+        </button>
 
-      <button>
-        <a href="https://github.com/tanveer-akhan">
-          <img src="/Assets/github_icon.svg" alt="Github" class="resume_icon" />
-        </a>
-      </button>
+        <button>
+          <a
+            href="/Assets/Docs/Tanveer_Ahmed_Khan_6_2026.pdf"
+            style="text-decoration: none"
+            >Resume</a
+          >
+        </button>
 
+        <button>
+          <a href="https://www.linkedin.com/in/tanveer-a-khan/">
+            <img
+              src="/Assets/linkedin_icon.svg"
+              alt="LinkedIn"
+              class="resume_icon"
+            />
+          </a>
+        </button>
 
+        <button>
+          <a href="https://github.com/tanveer-akhan">
+            <img
+              src="/Assets/github_icon.svg"
+              alt="Github"
+              class="resume_icon"
+            />
+          </a>
+        </button>
 
-      <button>
-        <a href="https://www.kaggle.com/lightaceing">
-          <img src="/Assets/kaggle_icon.svg" alt="Kaggle" class="resume_icon" />
-        </a>
-      </button>
+        <button>
+          <a href="https://www.kaggle.com/lightaceing">
+            <img
+              src="/Assets/kaggle_icon.svg"
+              alt="Kaggle"
+              class="resume_icon"
+            />
+          </a>
+        </button>
+      </div>
     </div>
     `;
   }
